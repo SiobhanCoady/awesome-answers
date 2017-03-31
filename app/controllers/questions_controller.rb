b4e41514cc3f7f3c2db553fe13797442a095cb7f
Siobhan Coady <siobhan.coady@gmail.com>
2017-03-31 11:04:44 -0700

    Refactor questions_controller, add notifications

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,12 @@
 class QuestionsController < ApplicationController
+
+  # The 'before_action' method registers another method. In this case it's the
+  # 'find_question' method, which will be executed just before the actions you
+  # specify in the 'only' array. Keep in mind that the method that gets executed
+  # as a 'before_action' happens within the save request/response cycle, so if
+  # you define an instance variable, you can use it within the action/views.
+  before_action :find_question, only: [:show, :edit, :update, :destroy]
+
   def new
     # we create a new 'Question' object because it will help us in
     # generating the form in 'view/new.html.erb'. We have to make it an
@@ -23,15 +31,25 @@ class QuestionsController < ApplicationController
     # the line below is what's called "Strong Parameters" feature that was added
     # to Rails starting with version 4 to help developers be more explicit about
     # the parameters that they want to allow the user to submit.
-    question_params = params.require(:question).permit([:title, :body])
+    # question_params = params.require(:question).permit([:title, :body])
     @question = Question.new question_params
     if @question.save
       # redirect_to question_path({id: @question.id})
       # redirect_to question_path(@question.id)
+
+      # Rails gives us access to 'flash' object, which looks like a Hash. flash
+      # utilizes cookies to store a bit of information that we can access in the
+      # next request. Flash will automatically be deleted when it is displayed.
+      flash[:notice] = 'Question created!'
       redirect_to question_path(@question)
       # render plain: 'Question created successfully'
     else
       # by default, Rails will try to render 'views/questions/create.html.erb'
+
+      # If you want to show a flash message when you're doing 'render' instead
+      # of 'redirect', meaning that you want to show the flash message within the
+      # same request/response cycle, then you will need to use flash.now
+      flash.now[:alert] = 'Please fix errors below'
       render :new
       # the above is the same as: render 'questions/new'
       # render plain: "Errors: #{question.errors.full_messages.join(', ')}"
@@ -50,7 +68,7 @@ class QuestionsController < ApplicationController
   #   "id": "245"
   # }
   def show
-    @question = Question.find params[:id]
+    # @question = Question.find params[:id]
     # render json: params
   end
 
@@ -59,22 +77,32 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    @question = Question.find params[:id]
+    # @question = Question.find params[:id]
   end
 
   def update
-    @question = Question.find params[:id]
-    question_params = params.require(:question).permit([:title, :body])
+    # @question = Question.find params[:id]
+    # question_params = params.require(:question).permit([:title, :body])
     if @question.update(question_params)
-      redirect_to question_path(@question)
+      redirect_to question_path(@question), notice: 'Question Deleted'
     else
       render :edit
     end
   end
 
   def destroy
-    question = Question.find params[:id]
-    question.destroy
-    redirect_to questions_path
+    # question = Question.find params[:id]
+    @question.destroy
+    redirect_to questions_path, notice: 'Question Deleted'
+  end
+
+  private
+
+  def find_question
+    @question = Question.find params[:id]
+  end
+
+  def question_params
+    params.require(:question).permit([:title, :body])
   end
 end
